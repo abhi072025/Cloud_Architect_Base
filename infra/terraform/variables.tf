fe54367de8089e8c7a7098931b1c8ddf88b21e1d
abhi072025 <abhighnagiri@gmail.com>
2025-11-28 13:44:49 +0530

    Add Terraform variables for prefix, location, and SQL admin

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,1 +1,17 @@
 
+variable "prefix" {
+  description = "Resource name prefix"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "southindia"
+}
+
+variable "sql_admin_password" {
+  description = "SQL admin password"
+  type        = string
+  sensitive   = true
+}
