01a2a2dcf92570ba4860ca04d7bf4f4383a4c548
abhi072025 <abhighnagiri@gmail.com>
2025-11-28 13:47:12 +0530

    Add outputs for resource group, AKS, ACR, and AGW IP

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,1 +1,16 @@
 
+output "resource_group" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "aks_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "acr_name" {
+  value = azurerm_container_registry.acr.name
+}
+
+output "agw_public_ip" {
+  value = azurerm_public_ip.agw_pip.ip_address
+}
